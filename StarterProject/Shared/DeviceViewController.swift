fede3d06395b7d0575c30ff010a1637999d6c77b
chen-bryan <chen.bryan20@gmail.com>
2017-08-03 13:45:24 -0700

    initial commit

---
+++ StarterProject/Shared/DeviceViewController.swift
@@ -0,0 +1,226 @@
+//
+//  DeviceViewController.swift
+//  SwiftStarter
+//
+//  Created by Stephen Schiffli on 10/20/15.
+//  Copyright © 2015 MbientLab Inc. All rights reserved.
+//
+
+import UIKit
+import MetaWear
+import AVFoundation
+
+class DeviceViewController: UIViewController {
+    
+    @IBOutlet weak var deviceStatus: UILabel!
+    @IBOutlet weak var headView: headViewController!
+    
+    let PI : Double = 3.14159265359
+    
+    var device: MBLMetaWear!
+    var timer : Timer?
+    var startTime : TimeInterval?
+    
+    var isPlaying: Bool = false
+    
+    var playSoundsController : PlaySoundsController!
+    var environment : Environment!
+    
+    struct Environment {
+        var name: String
+        var indexStart: Int
+        var numberOfSounds: Int
+    }
+    
+    
+    var seagullX : Float = -50
+    var seagullY : Float = 20
+    var seagullZ : Float = -5
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        device.addObserver(self, forKeyPath: "state", options: NSKeyValueObservingOptions.new, context: nil)
+        device.connectAsync().success { _ in
+            self.device.led?.flashColorAsync(UIColor.green, withIntensity: 1.0, numberOfFlashes: 3)
+            NSLog("We are connected")
+        }
+        // load Forest Environemnt into struc
+    }
+    
+    @IBAction func environment(_ sender: UIButton) {
+        isPlaying = true
+        switch sender.tag{
+            
+        case 0:
+            //forest
+            environment = Environment(name: "Forest", indexStart: 0, numberOfSounds: 3)
+            //print(sender.tag)
+            print ("y")
+            
+        case 1:
+            //beach
+            environment = Environment(name: "Beach", indexStart: 3, numberOfSounds: 2)
+            print ("z")
+        case 2:
+            //music school
+            environment = Environment(name: "Music School", indexStart: 5, numberOfSounds: 3)
+        case 3:
+            //cello
+            environment = Environment(name: "Cello", indexStart: 8, numberOfSounds: 12) //12
+        default: break
+        }
+        
+        loadSounds(env: environment)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        device.removeObserver(self, forKeyPath: "state")
+        device.led?.flashColorAsync(UIColor.red, withIntensity: 1.0, numberOfFlashes: 3)
+        device.disconnectAsync()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        OperationQueue.main.addOperation {
+            switch (self.device.state) {
+            case .connected:
+                self.deviceStatus.text = "Connected";
+                self.device.sensorFusion?.mode = MBLSensorFusionMode.imuPlus
+            case .connecting:
+                self.deviceStatus.text = "Connecting";
+            case .disconnected:
+                self.deviceStatus.text = "Disconnected";
+            case .disconnecting:
+                self.deviceStatus.text = "Disconnecting";
+            case .discovery:
+                self.deviceStatus.text = "Discovery";
+            }
+        }
+    }
+    
+    func getFusionValues(obj: MBLEulerAngleData){
+        
+        let xS =  String(format: "%.02f", (obj.p))
+        let yS =  String(format: "%.02f", (obj.y))
+        let zS =  String(format: "%.02f", (obj.r))
+    
+        let x = radians((obj.p * -1) + 90)
+        let y = radians(abs(365 - obj.y))
+        let z = radians(obj.r)
+        headView.setPointerPosition(w: 0.0, x : x, y: y, z: z)
+        playSoundsController.updateAngularOrientation(abs(Float(365 - obj.y)))
+
+        
+    }
+ 
+    func radians(_ degree: Double) -> Double {
+        return ( PI/180 * degree)
+    }
+    func degrees(_ radian: Double) -> Double {
+        return (180 * radian / PI)
+    }
+    
+    
+    @IBAction func startPressed(sender: AnyObject) {
+        if (isPlaying) {
+            device.sensorFusion?.eulerAngle.startNotificationsAsync { (obj, error) in
+                self.getFusionValues(obj: obj!)
+                }.success { result in
+                    print("Successfully subscribed")
+                }.failure { error in
+                    print("Error on subscribe: \(error)")
+            }
+        }
+    }
+    
+    @IBAction func stopPressed(sender: AnyObject) {
+        device.sensorFusion?.eulerAngle.stopNotificationsAsync().success { result in
+            print("Successfully unsubscribed")
+            }.failure { error in
+                print("Error on unsubscribe: \(error)")
+        }
+    }
+    
+    func loadSounds(env: Environment){
+        var soundArray : [String] = []
+        let start = env.indexStart
+        let num = env.indexStart + env.numberOfSounds - 1
+        for index in start...num{
+            soundArray.append(String(index) + ".wav")
+        }
+        playSoundsController = PlaySoundsController(file: soundArray)
+        
+        switch env.name{
+        case "Forest":
+            playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: 0, y: 0, z: -50))
+            //maybe can rotate around later w/timer like seagulls?
+            playSoundsController.updatePosition(index: 1, position: AVAudio3DPoint(x: 7.5, y: 10, z: 7.5 * sqrt(2.0)))
+            
+            //maybe play when gyromoves?
+            playSoundsController.updatePosition(index: 2, position: AVAudio3DPoint(x: 0, y: -2, z: 0))
+            
+        case "Beach":
+            playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: 0, y: 20, z: -15))
+            //seagulls: maybe can rotate around later w/timer
+            playSoundsController.updatePosition(index: 1, position: AVAudio3DPoint(x: 7.5, y: -10, z: 15 * sqrt(2.0)))
+            seagulls()
+        case "Music School":
+            playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: 0, y: 0, z: -15))
+            playSoundsController.updatePosition(index: 1, position: AVAudio3DPoint(x: 7.5*sqrt(3.0), y: 0, z: 7.5))
+            playSoundsController.updatePosition(index: 2, position: AVAudio3DPoint(x: -7.5 * sqrt(3.0), y: 0, z: 7.5))
+        case "Cello":
+            playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: 0, y: 0, z: -7.5))
+            playSoundsController.updatePosition(index: 1, position: AVAudio3DPoint(x: 3.25, y: 0, z: -3.25 * sqrt(3.0)))
+            playSoundsController.updatePosition(index: 2, position: AVAudio3DPoint(x: 3.25 * sqrt(3.0), y: 0, z: -3.25))
+            playSoundsController.updatePosition(index: 3, position: AVAudio3DPoint(x: 7.5, y: 0, z: 0))
+            playSoundsController.updatePosition(index: 4, position: AVAudio3DPoint(x: 3.25 * sqrt(3.0), y: 0, z: 3.25))
+            playSoundsController.updatePosition(index: 5, position: AVAudio3DPoint(x: 3.25, y: 0, z: 3.25*sqrt(3.0)))
+            playSoundsController.updatePosition(index: 6, position: AVAudio3DPoint(x: 0, y: 0, z: 7.5))
+            playSoundsController.updatePosition(index: 7, position: AVAudio3DPoint(x: -3.25, y: 0, z: 3.25*sqrt(3.0)))
+            playSoundsController.updatePosition(index: 8, position: AVAudio3DPoint(x: -3.25 * sqrt(3.0), y: 0, z:3.25))
+            playSoundsController.updatePosition(index: 9, position: AVAudio3DPoint(x: -7.5, y: 0, z: 0))
+            playSoundsController.updatePosition(index: 10, position: AVAudio3DPoint(x: -3.25 * sqrt(3.0), y: 0, z: -3.25))
+            playSoundsController.updatePosition(index: 11, position: AVAudio3DPoint(x: -3.25, y: 0, z: -3.25 * sqrt(3.0)))
+            
+        default: break
+        }
+
+        for sounds in soundArray.enumerated(){
+            // skip seagguls
+            if sounds.offset != 3 {
+                playSoundsController.play(index: sounds.offset)
+            }
+        }
+    
+    }
+    
+    func seagulls() {
+        timer = Timer()
+        //startTime = TimeInterval()
+        let aSelector : Selector = #selector(self.moveSoundsLinearPath)
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector,     userInfo: nil, repeats: true)
+        //startTime = Date.timeIntervalSinceReferenceDate
+        //play seagulls here
+        playSoundsController.play(index: 0)
+    }
+    
+    func moveSoundsLinearPath() {
+        print(seagullX)
+        playSoundsController.updatePosition(index: 0, position: AVAudio3DPoint(x: seagullX, y: seagullY, z: seagullZ))
+        seagullX += 0.1
+        if seagullX > 100.0 {
+            playSoundsController.stop(index: 0)
+            stopTimer()
+            seagullX = -100
+        }
+    }
+    
+    func stopTimer() {
+        if timer != nil {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+}
